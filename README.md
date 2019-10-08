ef3c49078b063b72364c765abb09c9fbf4982724
xuchengpeng <330276629@qq.com>
2019-10-08 18:50:36 +0800

    Customize hugo build

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Hugo博客
 
-[![Build Status](https://travis-ci.org/xuchengpeng/hugo-blog.svg?branch=master)](https://travis-ci.org/xuchengpeng/hugo-blog)
-[![](https://tokei.rs/b1/github/xuchengpeng/hugo-blog?category=lines)](https://github.com/xuchengpeng/hugo-blog)
+[![Build Status](https://travis-ci.org/xuchengpeng/xuchengpeng.github.io.svg?branch=master)](https://travis-ci.org/xuchengpeng/xuchengpeng.github.io)
--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,13 @@
 set -ev
 export TZ='Asia/Shanghai'
 
-git clone --branch=gh-pages https://${GH_REF} .deploy_git
-mv .deploy_git/.git/ ./public
+#git clone --branch=master https://${GH_REF} .deploy_git
+#mv .deploy_git/.git/ ./public
 
 cd ./public
+git init
 git config user.name "xuchengpeng"
 git config user.email "330476629@qq.com"
 git add .
 git commit -m "Site updated `date +"%Y-%m-%d %H:%M:%S"`"
-git push --force --quiet "https://${TRAVIS_TOKEN}@${GH_REF}" gh-pages:gh-pages
+git push --force --quiet "https://${TRAVIS_TOKEN}@${GH_REF}" master:master
